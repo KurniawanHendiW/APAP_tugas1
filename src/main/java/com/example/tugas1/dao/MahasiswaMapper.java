29104f4a750e5288fc64c8aed29ac9b37bd8694d
KurniawanHendiW <kurniawanhendiwijaya@gmail.com>
2018-03-18 09:43:37 +0700

    Update

--- src/main/java/com/example/tugas1/dao/MahasiswaMapper.java
+++ src/main/java/com/example/tugas1/dao/MahasiswaMapper.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 @Mapper
 public interface MahasiswaMapper {
-    @Select("select * from mahasiswa")
+    @Select("select * from mahasiswa m, program_studi p, fakultas f, universitas u where m.id_prodi=p.id and p.id_fakultas=f.id and f.id_univ=u.id")
     List<MahasiswaModel> selectAllMahasiswa();
 
-    @Select("select * from mahasiswa where npm = #{npm}")
+    @Select("select * from mahasiswa m, program_studi p, fakultas f, universitas u where m.id_prodi=p.id and p.id_fakultas=f.id and f.id_univ=u.id and m.npm = #{npm}")
     MahasiswaModel selectMahasiswa(@Param("npm") String npm);
 }
--- src/main/java/com/example/tugas1/model/MahasiswaModel.java
+++ src/main/java/com/example/tugas1/model/MahasiswaModel.java
@@ -13,11 +13,11 @@ public class MahasiswaModel {
     private String nama;
     private String tempat_lahir;
     private String tanggal_lahir;
-    private String jenis_kelamin;
+    private int jenis_kelamin;
     private String agama;
     private String golongan_darah;
     private String status;
     private int tahun_masuk;
     private String jalur_masuk;
-    private int id_prodi;
+    private ProgramStudiModel prodi;
 }
